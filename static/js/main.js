43bcc99a281e9d526fc1389d74d90b00b4cb5c21
Joseuribe1245 <81318473+Joseuribe1245@users.noreply.github.com>
2021-05-10 18:59:53 -0500

    avance

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,6 @@
-const btnDelete= document.querySelectorAll('.btn-delete');
-if(btnDelete) {
-  const btnArray = Array.from(btnDelete);
-  btnArray.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      if(!confirm('Are you sure you want to delete it?')){
-        e.preventDefault();
-      }
-    });
-  })
-}
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+	btnSwitch.classList.toggle('active');
+});
---
+++ static/js/sweetAlert.js
@@ -0,0 +1,14 @@
+Swal.fire({
+	title: "BIENVENIDO",
+	text: "Espero que te guste nuestra aplicacion",
+	icon:"success",
+	width:"25%",
+	padding: "5rem",
+	allowOutsideClick: false,
+	allowEscapeKey: false,
+	allowEnterKey:false,
+	stopKeydownPropagation: false,
+	imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Unalm_logo.png",
+
+	imageAlt: "Agraria",
+})
